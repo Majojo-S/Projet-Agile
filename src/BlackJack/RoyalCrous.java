c9bf3f4f0f5b2961755e7bfaa2355a9aec4bc602
Theo Franos <theo.franos.etu@univ-lille.fr>
2022-09-02 14:22:49 +0200

    jeu + victoire/defaite + recommencer le jeu

--- src/BlackJack/RoyalCrous.java
+++ src/BlackJack/RoyalCrous.java
@@ -4,10 +4,11 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
 import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
 
 public class RoyalCrous {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         System.out.println("Voulez-vous commencer une partie ? Entrez oui ou non");
 
         Scanner scanner = new Scanner(System.in);
@@ -33,34 +34,62 @@ public class RoyalCrous {
         int nb = scanner.nextInt();
         Packet jeu = new Packet(nb);
         int choix = 0;
-        player.hand.add(jeu.PickCard());
-    	player.hand.add(jeu.PickCard());
-    	player.croupier.add(jeu.PickCard());
-    	System.out.println("Votre main est : " + player.hand + "total : " + player.totalOfHand() + "\n");
-    	System.out.println("La carte du croupier est : " + player.croupier + "total : "+ player.totalOfCroupier()+ "\n");
-    	System.out.println("Voulez-vous re-piochez ? 1 ou 2");
-    	choix = scanner.nextInt();
-    	System.out.println(choix);
-        while(choix != 0 || choix != 0) {
-        	if(choix == 0) {
-        		System.out.println("Vous avez perdu le Croupier Gagne");
-        		choix = 0;
-        	} else if(choix == 1 && player.totalOfHand() < 21){
-        		player.hand.add(jeu.PickCard());
-            	System.out.println("Votre main est : " + player.hand + "total : " + player.totalOfHand());
-            	if(player.totalOfHand() > 21) {
-            		System.out.println("Vous avez perdu le Croupier Gagne");
-            		choix = 0;
-            	}
-            	System.out.println("Voulez-vous re-piochez ? 1 ou 2");
-            	choix = scanner.nextInt();
-        	} else if (choix == 2) {
-        		
-        	} else if (player.totalOfHand() == 21 && player.totalOfCroupier() < player.totalOfHand()) {
-        		System.out.println("Vous avez gagné");
-        		choix = 0;
-        	}
+        boolean fin = false;
+        while(fin != true) {
+        	System.out.println("Début du jeu ! ");
+        	clear();
+        	player.hand.clear();
+        	player.croupier.clear();
+        	player.hand.add(jeu.PickCard());
+	    	player.hand.add(jeu.PickCard());
+	    	player.croupier.add(jeu.PickCard());
+	    	System.out.println("Votre main est : " + player.hand + "total : " + player.totalOfHand() + "\n");
+	    	System.out.println("La carte du croupier est : " + player.croupier + "total : "+ player.totalOfCroupier()+ "\n");
+	    	if(player.totalOfHand() == 21) {
+	    		System.out.println("BLACKJACK ! Vous remportez cette partie !");
+	    		choix = 0;
+	    	}
+	    	System.out.println("1 pour piocher et 2 pour rester");
+	    	choix = scanner.nextInt();
+	        while(choix != 0) {
+	        	if(choix == 0) {
+	        		System.out.println("Vous avez perdu le Croupier Gagne");
+	        		choix = 0;
+	        		fin = true;
+	        	} else if(choix == 1 && player.totalOfHand() < 21){
+	        		player.hand.add(jeu.PickCard());
+	            	System.out.println("Votre main est : " + player.hand + "total : " + player.totalOfHand());
+	            	if(player.totalOfHand() > 21) {
+	            		System.out.println("Vous avez perdu le Croupier Gagne");
+	            		choix = 0;
+	            		TimeUnit.SECONDS.sleep(3);
+	            	} else {
+	            		System.out.println("1 pour piocher et 2 pour rester");
+	                	choix = scanner.nextInt();
+	            	}
+	        	} else if (choix == 2) {
+	        		while(player.totalOfCroupier() < 17) {
+	        			player.croupier.add(jeu.PickCard());
+	        			System.out.println("Les cartes du croupier est : " + player.croupier + "total : "+ player.totalOfCroupier()+ "\n");
+	        		}
+	        		if (player.totalOfCroupier() < player.totalOfHand() || player.totalOfCroupier() > 21) {
+	            		System.out.println("Vous avez gagné");
+	            		choix = 0;
+	            		TimeUnit.SECONDS.sleep(3);
+	            	} else if(player.totalOfHand() > 21 || player.totalOfCroupier() > player.totalOfHand() && player.totalOfCroupier() < 21) {
+	            		System.out.println("Vous avez perdu le Croupier Gagne");
+	            		choix = 0;
+	            		TimeUnit.SECONDS.sleep(3);
+	            	} else if(player.totalOfCroupier() == player.totalOfHand()) {
+	            		System.out.println("Egalité vous recuperez votre mise");
+	            		choix = 0;
+	            		TimeUnit.SECONDS.sleep(3);
+
+	            	}
+	        	}
+	        }
         }
+	        
         scanner.close();
     }
 
@@ -80,11 +109,14 @@ public class RoyalCrous {
         player.echelon = echelon;
         player.bourse = bourse;
 
-        System.out.println("Le crous vous a attribué l'échelon " + echelon + " \nVotre bourse s'élève à " + bourse);
-
-
-        
+        System.out.println("Le crous vous a attribué l'échelon " + echelon + " \nVotre bourse s'élève à " + bourse);  
     }
+    
+    private static void clear(){
+		for (int i = 0; i < 14; i++) {
+			System.out.println("\n");
+		}
+	}
 
     
 }
