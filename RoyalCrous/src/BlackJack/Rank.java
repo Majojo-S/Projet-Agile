298edf81d780a1d12e78af9cb4c3eede8be4756a
Marine Sandras <marine.sandras.etu@univ-lille.fr>
2022-09-02 08:43:37 +0200

    conflit rank résou

--- RoyalCrous/src/BlackJack/Rank.java
+++ RoyalCrous/src/BlackJack/Rank.java
@@ -1,5 +1,13 @@
 package BlackJack;
 
 enum Rank {
-	DEUX, TROIS, QUATRE, CINQ, SIX, SEPT, HUIT, NEUF, DIX ,VALET, DAME, ROI, AS;
+
+	DEUX(2), TROIS(3), QUATRE(4), CINQ(5), SIX(6), SEPT(7), HUIT(8), NEUF(9), VALET(10), ROI(13), DAME(12), ACE(1);
+	
+	
+	int points;
+	Rank(int i) {
+		points = i;
+	}
+	
 }
