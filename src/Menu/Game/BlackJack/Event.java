4ebbaced0f189ad4f6b09be5cdaf5a2982c15457
Mounir Khatri <mounir.khatri.etu@univ-lille.fr>
2022-09-05 16:37:02 +0200

    updt

--- src/Menu/Game/BlackJack/Event.java
+++ src/Menu/Game/BlackJack/Event.java
@@ -9,7 +9,10 @@ public class Event {
 	public Event(int timer) {
 		this.timer = timer;
 	}
-	
+	public String red= "\033[0;31m";
+	public String green = "\033[0;32m";
+	public static final String ANSI_RESET = "\u001B[0m";
+	public String blue="\033[0;34m";
 	public int getTimer() {
 		return this.timer;
 	}
@@ -20,7 +23,7 @@ public class Event {
 	
 	public void Loyer(Player p) {
 		if(timer == 30) {
-			System.out.println("Il est temps de payer ses factures ! \\n");
+			System.out.println("Il est temps de payer ses factures ! \n");
 			if(p.echelon < 3) {
 				p.setBourse(p.getBourse() - 100);
 			} else {
@@ -31,7 +34,7 @@ public class Event {
 	
 	public void Bourses(Player p) {
 		if(timer == 31) {
-			System.out.println("Voila votre Bourse du mois ! \\n");
+			System.out.println("Voila votre Bourse du mois ! \n");
 			p.setBourse(p.getBourse() + 250);
 		}
 	}
@@ -40,34 +43,40 @@ public class Event {
 		int min = 50;
 		int max = 300;
 		Random rand = new Random();
-		System.out.println("C'est l'heure de l'évenement mystère !! \\n");
+		System.out.println("C'est l'heure de l'évenement mystère !! \n");
 		if(rte.equals(RandomTypeEvent.PH)) {
-			p.setBourse(p.getBourse() + rand.nextInt((max - min)+1)-min);
-			System.out.println("Vous recevez un virement de vos parents !" + rand.nextInt((max - min)+1) + "de crédits ps: remerciez les quand même \\n");
+			p.setBourse(p.getBourse() + rand.nextInt(max)+min);
+			System.out.println (green +" Vous recevez un virement de vos parents !" + (rand.nextInt(max)+min) + "de crédits ps: remerciez les quand même \n" + ANSI_RESET);
 		} else if(rte.equals(RandomTypeEvent.WORK)) {
-			System.out.println("Pour votre bon service, le CROUS vous octroie un salaire pour le travail réalisé ! + 150 crédits \\n");
+			System.out.println( green+"Pour votre bon service, le CROUS vous octroie un salaire pour le travail réalisé ! + 150 crédits \n"+ANSI_RESET);
 		} else if(rte.equals(RandomTypeEvent.SUP)) {
-			p.setBourse(p.getBourse() + rand.nextInt((max - min)+1)-min);
-			System.out.println("C'est votre jour de chance !!! Le CROUS vous à selectionez pour vous attribuez un supplément de bourse !! + " + rand.nextInt((max - min)+1) + "de solde pour vous ! \\n");
+			p.setBourse(p.getBourse() + rand.nextInt(max)+min);
+			System.out.println(green+"C'est votre jour de chance !!! Le CROUS vous à selectionez pour vous attribuez un supplément de bourse !! + " + (rand.nextInt(max)+min) + "de solde pour vous ! \n"+ANSI_RESET);
 		} else if(rte.equals(RandomTypeEvent.LUNCH)) {
 			p.setBourse(p.getBourse() - 50);
-			System.out.println("Les repas du CROUS vous ont était facturé... -50 crédits");
+			System.out.println(red+"Les repas du CROUS vous ont était facturé... -50 crédits \n"+ANSI_RESET);
 		} else if(rte.equals(RandomTypeEvent.ECHELON)) {
 			p.setEchelon(rand.nextInt(6)+1);
-			System.out.println("Allez vous avoir de la chance ou non ? allez vous prendre un echelon ou plutot en perdre, ah ah t'elle est la question");
+			System.out.println(blue+"Allez vous avoir de la chance ou non ? allez vous prendre un echelon ou plutot en perdre, ah ah t'elle est la question \n"+ANSI_RESET);
 			TimeUnit.SECONDS.sleep(3);
-			System.out.println("Vous êtes desormais echelon" + p.getEchelon() + "\\n");
+			System.out.println("Vous êtes desormais echelon" + p.getEchelon() + "\n");
 		} else if(rte.equals(RandomTypeEvent.BAR)) {
 			p.setBourse(p.getBourse() - 75);
-			System.out.println("Aie grosse soirée hier hein ? Consommez avec modération... - 75 crédits pour vous \\n");
+			System.out.println(red+"Aie grosse soirée hier hein ? Consommez avec modération... - 75 crédits pour vous \n"+ANSI_RESET);
 		} else if(rte.equals(RandomTypeEvent.AMENDE)) {
 			p.setBourse(p.getBourse() - 50);
-			System.out.println("OOF, la grosse amende dans les dents, attention la prochaine fois... - 50 crédits \\n");
+			System.out.println(red+"OOF, la grosse amende dans les dents, attention la prochaine fois... - 50 crédits \n"+ANSI_RESET);
 		} else if(rte.equals(RandomTypeEvent.ABSENT)) {
 			p.setBourse(p.getBourse() - 150);
+<<<<<<< HEAD
 			System.out.println("Le CROUS n'est pas contend la, vous avez était trop absent en cour récemment ils vous enlèvent donc 150 crédits (plutôt que vous enlevez votre bourse) \\n");
 		} else {
 			System.out.println("Bah non y'a rien");
+=======
+			System.out.println(red+"Le CROUS n'est pas content la, vous avez était trop absent en cour récemment ils vous enlèvent donc 150 crédits (plutôt que vous enlevez votre bourse) \n"+ANSI_RESET);
+		} else {
+			System.out.println(blue+"Bah non ya rien"+ANSI_RESET);
+>>>>>>> 0781594 (updt)
 		}
 	}
 	
