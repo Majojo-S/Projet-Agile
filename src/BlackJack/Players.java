97b1af1bcd8c63a3d54d41c1a9aaa5e495212154
Hocine ALIOUAT <hocine.aliouat.etu@epicea05.iut-infobio.priv.univ-lille1.fr>
2022-09-05 09:56:49 +0200

    fix text

--- src/BlackJack/Players.java
+++ src/BlackJack/Players.java
@@ -133,9 +133,6 @@ public class Players {
 	
 	public void updateTxt() {
 		
-		  for(Player e:players) {
-		   System.out.println(e);
-		 }
 		try (FileWriter fw=new FileWriter(this.f)){
 			StringBuilder sb=new StringBuilder();
 			for(Player player:players) {
